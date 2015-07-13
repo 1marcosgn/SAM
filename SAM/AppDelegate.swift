d837d8dc2a2fdbcab2dcdfa9d11fedcb1bf51025
Marcos Garcia <marcos.x.garcia.-nd@disney.com>
2015-07-13 14:16:37 -0700

    Basic structure for slide menu controller

--- SAM/AppDelegate.swift
+++ SAM/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SlideMenuControllerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,8 +16,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        let menuController = MenuViewController()
+        let mainController = CollectionsViewController()
+        let navControlMain = UINavigationController(rootViewController: mainController)
+        let navControlMenu = UINavigationController(rootViewController: menuController)
+        
+        let slideMenuController = SlideMenuController(mainViewController: navControlMain, leftMenuViewController: navControlMenu)
+        
+        self.window?.rootViewController = slideMenuController
+        self.window?.makeKeyAndVisible()
+
         return true
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
